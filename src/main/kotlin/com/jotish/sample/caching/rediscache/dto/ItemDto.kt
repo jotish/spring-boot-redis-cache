10657292f989966c956b3d8c60403fc6b939728b
Jotish Suthar <jotishsuthar@PE-BGLR-679.local>
2022-08-16 18:37:35 +0530

    Add inline comment for using @cacheable on the DTO

--- src/main/kotlin/com/jotish/sample/caching/rediscache/dto/ItemDto.kt
+++ src/main/kotlin/com/jotish/sample/caching/rediscache/dto/ItemDto.kt
@@ -6,6 +6,9 @@ import java.io.Serializable
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Cacheable
+// @Cacheable is must if you want to cache type information while using @Cacheable
+// Refer this for more details
+// https://stackoverflow.com/questions/52265326/how-can-i-easily-cache-kotlin-objects-in-redis-using-json-via-jackson
 data class ItemDto(
     var id: Int?, var description: String?
 ) : Serializable {
